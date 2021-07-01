a475b208adfa651bc1a310e6819901c2f93d35d1
Subhajit <mandalsubhajit655@gmail.com>
2021-07-01 12:29:24 +0530

    Add comments

--- SL_ListApp.java
+++ SL_ListApp.java
@@ -1,11 +1,16 @@
 public class SL_ListApp {
     public static void main(String[] args) {
         SinglyLinkedList lList = new SinglyLinkedList();
+        
+        // Add element at tail using 'add' method
         for(int i = 0; i < 10; i++){
             lList.add(i);
         }
+        
         System.out.println("\n");
+        
         Node head = lList.head;
+        // insert element tail using 'insertNodeAtTail' method (hackerrank sol.)
         for(int i = 10; i < 20; i++){
             head = lList.insertNodeAtTail(head, i);
         }
